d684dbaba1483902ab6f9ff84aab74a64bbd562a
ice19942335 <ice19942335@gmail.com>
2019-11-26 10:06:57 +0000

    Added InstallServicesAssembly and IInstaller

---
+++ ELA_Data_Service/Installers/ExtensionsInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ELA_Data_Service.Installers.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ELA_Data_Service.Installers
+{
+    public static class ExtensionsInstaller
+    {
+        public static void InstallServicesAssembly(this IServiceCollection services, IConfiguration configuration)
+        {
+            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
+                    typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+                .Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+
+            installers.ForEach(installer => installer.InstallServices(services, configuration));
+        }
+    }
+}
