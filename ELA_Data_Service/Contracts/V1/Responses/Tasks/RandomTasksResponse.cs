9c3a62d657b0e9ebabdfe2e4e6624331e0d49499
ice19942335 <ice19942335@gmail.com>
2019-11-26 23:19:26 +0000

    RandomTasksResponse Refactor

--- ELA_Data_Service/Contracts/V1/Responses/Tasks/RandomTasksResponse.cs
+++ ELA_Data_Service/Contracts/V1/Responses/Tasks/RandomTasksResponse.cs
@@ -10,26 +10,6 @@ namespace ELA_Data_Service.Contracts.V1.Responses.Tasks
 {
     public class RandomTasksResponse
     {
-        public int TaskId { get; set; }
-
-        public int Points { get; set; }
-
-        public int GivenWordId { get; set; }
-
-        public string TaskType { get; set; }
-
-        public string TaskDescription { get; set; }
-
-        public string ImgUrl { get; set; }
-
-        public string Transcription { get; set; }
-
-        public string GivenWordEng { get; set; }
-
-        public string GivenWordRus { get; set; }
-
-        public IEnumerable<ElaGivenAnswerDto> GivenAnswers { get; set; }
-
-        public IEnumerable<string> Errors { get; set; }
+        public IEnumerable<ElaTaskDto> Tasks { get; set; }
     }
 }
