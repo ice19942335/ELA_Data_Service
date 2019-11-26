f0d7708e177f495ece6f84fd0906b58c9659fbc0
ice19942335 <ice19942335@gmail.com>
2019-11-26 11:09:56 +0000

    Added ApiRoutes

---
+++ ELA_Data_Service/Controllers/V1/TasksController.cs
@@ -0,0 +1,21 @@
+﻿using ELA_Data_Service.Contracts.V1;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELA_Data_Service.Controllers.V1
+{
+    [EnableCors]
+    [Produces("application/json")]
+    [Authorize(JwtBearerDefaults.AuthenticationScheme)]
+    public class TasksController : Controller
+    {
+        [HttpGet(ApiRoutes.Tasks.RandomTasks)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
